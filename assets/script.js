f7314aa554256b0c701f06409f117e6323af6c33
larsonrj <rylarson17@gmail.com>
2022-11-13 19:27:11 -0700

    Updated functions to store city name

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,21 @@
 var searchBtnEl = $("#search-btn");
 var searchCityEl = $("#city-search");
+var storedCityEl = $(".stored-city")
 var apiKey = "&appid=c4e7bb5dffaa5190296f9adfad6f11dd";
+const storedCity = [];
+
+function populateCities (){
+    storedCityEl.each(function (i) {
+    index=i+1;
+    var text = localStorage.getItem("city-" + index);
+    $(this).text(text);
+})
+};
 
 searchBtnEl.click(function () {
     var city = searchCityEl.val();
     var urlRequest = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
-
+    localStorage(city);
     fetch(urlRequest)
     .then(function (response) {
         return response.json();
@@ -15,6 +25,7 @@ searchBtnEl.click(function () {
             lat: data.city.coord.lat,
             lon: data.city.coord.lon
         }
+        
         
         fiveDay(coordinates)
     })
@@ -32,3 +43,15 @@ function fiveDay(coordinates) {
       .then(function (dayData) {
         console.log(dayData)})
       }
+
+function localStorage (city) {
+    if (storedCity.length < 8) {
+        storedCity.unshift(city);
+    }
+    else {
+        storedCity.pop();
+        storedCity.unshift(city);
+    };
+    };
+
+
