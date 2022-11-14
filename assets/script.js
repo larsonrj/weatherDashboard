76a4031f061c5f5630bf668a34ec63b54ec6cd0b
larsonrj <rylarson17@gmail.com>
2022-11-13 21:14:20 -0700

    Stored city names are working

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
-var searchBtnEl = $("#search-btn");
+var searchBtnEl = $(".btn");
 var searchCityEl = $("#city-search");
 var storedCityEl = $(".stored-city")
 var apiKey = "&appid=c4e7bb5dffaa5190296f9adfad6f11dd";
-const storedCity = [];
+var storedCity = [localStorage.getItem("city-1"), localStorage.getItem("city-2"), localStorage.getItem("city-3"), localStorage.getItem("city-4"), localStorage.getItem("city-5"), localStorage.getItem("city-6"), localStorage.getItem("city-7"), localStorage.getItem("city-8")]
+
 
 function populateCities (){
     storedCityEl.each(function (i) {
@@ -12,10 +13,33 @@ function populateCities (){
 })
 };
 
+console.log(searchCityEl.val())
+populateCities()
+
 searchBtnEl.click(function () {
-    var city = searchCityEl.val();
+    if ($(this).attr("id") =="search-btn") {
+    var city = searchCityEl.val();}
+    else var city = $(this).text();
+
     var urlRequest = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
-    localStorage(city);
+
+    if (storedCity.length < 8) {
+        storedCity.unshift(city);
+    }
+    else {
+        storedCity.pop();
+        storedCity.unshift(city);
+    };
+    
+    for (var i=0; i<storedCityEl.length; i++) {
+    var index = i+1;
+    var storedID = "city-" + index;
+    console.log(storedID)
+    localStorage.setItem(storedID, storedCity[i])
+    };
+
+    populateCities();
+
     fetch(urlRequest)
     .then(function (response) {
         return response.json();
@@ -43,15 +67,3 @@ function fiveDay(coordinates) {
       .then(function (dayData) {
         console.log(dayData)})
       }
-
-function localStorage (city) {
-    if (storedCity.length < 8) {
-        storedCity.unshift(city);
-    }
-    else {
-        storedCity.pop();
-        storedCity.unshift(city);
-    };
-    };
-
-
