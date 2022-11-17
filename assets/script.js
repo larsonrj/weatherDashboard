ea5f59bea69cbf8fe1d389b40fa10ee1705a7073
Ryan <rylarson17@gmail.com>
2022-11-17 09:29:20 -0700

    All weather data rendering on page from search

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,122 @@
 var searchBtnEl = $(".btn");
 var searchCityEl = $("#city-search");
-var storedCityEl = $(".stored-city")
+var storedCityEl = $(".stored-city");
+var todayListEl = $(".today");
 var apiKey = "&appid=c4e7bb5dffaa5190296f9adfad6f11dd";
-var storedCity = [localStorage.getItem("city-1"), localStorage.getItem("city-2"), localStorage.getItem("city-3"), localStorage.getItem("city-4"), localStorage.getItem("city-5"), localStorage.getItem("city-6"), localStorage.getItem("city-7"), localStorage.getItem("city-8")]
+var storedCity = [
+  localStorage.getItem("city-1"),
+  localStorage.getItem("city-2"),
+  localStorage.getItem("city-3"),
+  localStorage.getItem("city-4"),
+  localStorage.getItem("city-5"),
+  localStorage.getItem("city-6"),
+  localStorage.getItem("city-7"),
+  localStorage.getItem("city-8"),
+];
 
-
-function populateCities (){
-    storedCityEl.each(function (i) {
-    index=i+1;
+function populateCities() {
+  storedCityEl.each(function (i) {
+    index = i + 1;
     var text = localStorage.getItem("city-" + index);
     $(this).text(text);
-})
-};
+    if (storedCityEl.eq(i).text() == "") {
+      storedCityEl.eq(i).hide();
+    } else if (storedCityEl.eq(i).text() == "null") {
+      storedCityEl.eq(i).hide();
+    } else {
+      storedCityEl.eq(i).show();
+    }
+  });
+}
 
-console.log(searchCityEl.val())
-populateCities()
+populateCities();
 
 searchBtnEl.click(function () {
-    if ($(this).attr("id") =="search-btn") {
-    var city = searchCityEl.val();}
-    else var city = $(this).text();
+  if ($(this).attr("id") == "search-btn") {
+    var city = searchCityEl.val();
+  } else {
+    var city = $(this).text();
+  }
 
-    var urlRequest = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
+  var urlRequest =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    apiKey +
+    "&units=imperial";
 
-    if (storedCity.length < 8) {
-        storedCity.unshift(city);
-    }
-    else {
-        storedCity.pop();
-        storedCity.unshift(city);
-    };
-    
-    for (var i=0; i<storedCityEl.length; i++) {
-    var index = i+1;
+  if (storedCity.length < 8) {
+    storedCity.unshift(city);
+  } else {
+    storedCity.pop();
+    storedCity.unshift(city);
+  }
+
+  for (var i = 0; i < storedCityEl.length; i++) {
+    var index = i + 1;
     var storedID = "city-" + index;
-    console.log(storedID)
-    localStorage.setItem(storedID, storedCity[i])
-    };
+    localStorage.setItem(storedID, storedCity[i]);
+  }
 
-    populateCities();
+  populateCities();
 
-    fetch(urlRequest)
+  fetch(urlRequest)
     .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        
-        var coordinates = {
-            lat: data.city.coord.lat,
-            lon: data.city.coord.lon
-        }
-        
-        
-        fiveDay(coordinates)
+      return response.json();
     })
-})
+    .then(function (data) {
+      var coordinates = {
+        lat: data.coord.lat,
+        lon: data.coord.lon,
+      };
+      console.log(data);
+      var convertDate = new Date(data.dt * 1000);
+      var date = convertDate.toLocaleDateString();
+      todayListEl.eq(0).text(data.name + " " + date);
+      todayListEl.eq(1).text("Temp: " + data.main.temp + "\u00B0F");
+      todayListEl.eq(2).text("Wind: " + data.wind.speed + "MPH");
+      todayListEl.eq(3).text("Humidity: " + data.main.humidity + "%");
+      fiveDay(coordinates);
+    });
+});
 
 function fiveDay(coordinates) {
-    var lat = coordinates.lat;
-    var lon = coordinates.lon;
-    fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + apiKey + "&cnt=5";
-    
-    fetch(fiveDayURL)
+  var lat = coordinates.lat;
+  var lon = coordinates.lon;
+  fiveDayURL =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    apiKey +
+    "&units=imperial";
+
+  fetch(fiveDayURL)
     .then(function (response) {
-        return response.json();
-      })
-      .then(function (dayData) {
-        console.log(dayData)
-        $(".today li:nth-child(1)").text()
-        var weekCast = $(".week")
-    }
-    )
+      return response.json();
+    })
+    .then(function (dayData) {
+      console.log(dayData);
+      var dayEl = $(".day");
+      var tempEl = $(".temp");
+      var windEl = $(".wind");
+      var humidEl = $(".humid");
+      dayEl.each(function (i) {
+        let index = i * 8;
+        var convertDate = new Date(dayData.list[index].dt * 1000);
+        var date = convertDate.toLocaleDateString();
+        $(this).text(date);
+      });
+      tempEl.each(function (i) {
+        let index = i * 8;
+        $(this).text("Temp: " + dayData.list[index].main.temp + "\u00B0F");
+      });
+      windEl.each(function (i) {
+        let index = i * 8;
+        $(this).text("Wind: " + dayData.list[index].wind.speed + "MPH");
+      });
+      humidEl.each(function (i) {
+        let index = i * 8;
+        $(this).text("Humidity: " + dayData.list[index].main.humidity + "%");
+      });
+    });
 }
